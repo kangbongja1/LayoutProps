bb52b1b5ee336690e86972052ba61ce773550270
kangbongja1 <nbhommeprize@hufs.ac.kr>
2020-10-07 11:21:48 +0900

    Test Text Image

--- App.js
+++ App.js
@@ -1,14 +1,30 @@
 import React, {useState} from 'react';
-import {Button, View, StyleSheet, ScrollView} from 'react-native';
+import {Image, Text, Button, View, StyleSheet, ScrollView} from 'react-native';
 import Constants from 'expo-constants';
+
+
+
 
 const App = () => {
   return (
     <>
       <View style={{ paddingTop: Constants.statusBarHeight}} />
       <View style={[styles.container, styles.playingSpace]}>
+        <Text>테스트입니다. dar dar dar</Text>
 
+        <Image style ={{
+          width: 300,
+          height: 300,
+          resizeMode: 'contain'
+        }}
+        
+        
+        source={{
+          url: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxASERMQDxAPDxAPDw8PDw0NDw8NDQ8PFREWFhURFRUYHSggGBolHhUVITEhJSkrLi4uFx8zODMvNyguLisBCgoKDg0OGhAQFy0dHR0rLS0tLS0tLy0tLSstLS0tLS0tLS0tLS0tKystLS0tLSsrLS0rLS0tLS0tKy0tKy0tLf/AABEIAMABBgMBIgACEQEDEQH/xAAcAAACAwEBAQEAAAAAAAAAAAADBAECBQAGBwj/xABGEAACAQMCAwQHAwgHCAMAAAABAgADESEEEgUxURMiQWEGBzJxgZHSF1SUFCNCk6Gx0fAVJDNTwcLhQ1JicoKEovEWNET/xAAaAQADAQEBAQAAAAAAAAAAAAABAgQDAAUG/8QAJREAAgICAQMFAQEBAAAAAAAAAAECEQMhEgQxURMiQVKRFFNC/9oADAMBAAIRAxEAPwDxN
+        }}
+        />
       </View>
+
       <ScrollView style={[styles.container]}>
         <View style={[styles.controlSpace]}>
           <View style={styles.buttonView}>            
@@ -42,6 +58,8 @@ const App = () => {
   );
 };
 
+
+
 const styles = StyleSheet.create({
   container: {
     height: '50%',
@@ -62,5 +80,8 @@ const styles = StyleSheet.create({
     padding: 10,
   }
 })
+
+
+
 
 export default App;
